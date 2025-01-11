5a344a3b42c7237558f22ce55a2b288d15048be9
Your Name <khalidsaifullaha3@gmail.com>
2025-01-11 21:02:02 +0600

    contact page button color chang

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
         </div>
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+          className="w-full bg-gray-700  hover:bg-gray-600 text-white py-3 px-4 rounded-lg shadow-md  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
         >
           Send Message
         </button>
